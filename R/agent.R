49b603bb9af061749473d0882eaf862e446d2289
Marcus Thomson <38635903+zizroc@users.noreply.github.com>
2021-04-02 11:25:09 -0700

    Update agent.R
    Added (1) agents list; (2) started an append_agents function (check with unit test to see if it works)

--- R/agent.R
+++ R/agent.R
@@ -4,6 +4,7 @@
 #' @description This is an object that represents an abstract agent.
 #' @details This class is the basic agent in the framework.
 #' @field identifier A unique identifier that can be used to identify the agent
+#' @field agents A list of agents
 #' @field age A time-step dependent variable
 #' @section Methods:
 #' \describe{
@@ -12,31 +13,39 @@
 #'   \item{\code{get_id()}}{Returns unique identifier}
 #'   \item{\code{initialize()}}{Create a new agent}
 #'   \item{\code{propagate()}}{Runs every time-step}
+#'   \item{\code{append_agent()}}{Appends new agent to list of agents}
 #'   }
 agent <- R6::R6Class(
   "agent",
   public = list(
-    identifier = NULL,
+    identifier = NULL, 
+    agents = NULL, 
     #' Create a new agent
     #'
     #' @description Used to created new agent objects.
     #'
     #' @export
     #' @param identifier The agent's unique identifier
-    #' @return A new agent object
+    #' @return A new agent object, 
     initialize = function(
-      identifier = NA,
-      age        = 0
+      identifier = NA, 
+      agents = list(), 
+      age = 0
     ) {
       if (is.na(identifier)) {
         library(uuid)
         identifier <- uuid::UUIDgenerate()
       }
       self$identifier <- identifier
+      self$agents <- agents
     },
     #' Agent-level time dependence
     #'
     #' @return None
     propagate = function() {
    }
+#    , 
+#    append_agent = function(value) { 
+#      agents <- append_agent(agents, value)
+#   }
   ))
