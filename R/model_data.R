c3381314965199cd36cb8c5b5b6fbe05bd1ace40
Thomas Thelen <tommythelen@gmail.com>
2021-06-25 19:07:59 -0700

    Add R6 documentation for model_data

--- R/model_data.R
+++ R/model_data.R
@@ -1,20 +1,13 @@
-#' @export
-#' @title Model Data
-#' @docType class
-#' @description This is an object that holds data meant to persist outside ofe the model itself.
-#' @details Sometimes a model event occurs without any great description in the villager framework. When this happens
-#' an alternative to subclassing is by placing the data in the model_data instance which can be accessed each day.
-#' @field events A list of events
-#' @section Methods:
-#' \describe{
-#'   }
+#' R6 Class representing data that's external from resources and winiks
+#'
+#' It contains a single variable, 'events' for when the data holds a list of events
 model_data <- R6::R6Class("model_data",
-                     public = list(events=NULL,
-                                   #' Create a new model_data
-                                   #'
-                                   #' @description Instantiates a model_data class.
-                                   #' @return A new model data object
-                                   initialize = function() {
-                                     self$events <- list()
-                                   }
-                     ))
+                          public = list(
+                            #' @field events Any events that need to be tracked
+                            events=NULL,
+                            #' @description Creates a new model_data object
+                            #' @return A new model data object
+                            initialize = function() {
+                              self$events <- list()
+                            }
+                          ))
